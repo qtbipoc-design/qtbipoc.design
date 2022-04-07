89d965695b48fc77ae354790d6e5fe6da4883aab
ivanqtbipocdesign <ivan@qtbipoc.design>
2022-04-07 14:36:51 -0700

    Fix: Set code section to run after document ready

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,17 @@ console.log('Main Loaded');
 
 
 // This wraps the string of the Search & Filter result count in a heading tag
-(function($){
-  if ($('.search-filter-results')) {
-    // console.log('results exists');
-    $('.search-filter-results')
-      .contents()
-      .filter(function () {
-        return this.nodeType === 3;
-      })
-      .eq(0)
-      .wrap('<h4 class="search-filter-results-heading"></h4>');
-  }
+(function ($) {
+  $(function () {
+    if ($('.search-filter-results')) {
+      console.log('results exists');
+      $('.search-filter-results')
+        .contents()
+        .filter(function () {
+          return this.nodeType === 3;
+        })
+        .eq(0)
+        .wrap('<h4 class="search-filter-results-heading"></h4>');
+    }
+  });
 })(jQuery);
