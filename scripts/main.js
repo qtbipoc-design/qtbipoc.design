8b25de22f08fe4af255918ac675b5308729d9382
ivanqtbipocdesign <ivan@qtbipoc.design>
2022-05-05 13:17:10 -0700

    Chore: Clean old code

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,1 @@
 console.log('Main Loaded');
-
-
-/*
-// This wraps the string of the Search & Filter result count in a heading tag
-(function ($) {
-  $(function () {
-    if ($('.search-filter-results')) {
-      $('.search-filter-results')
-        .contents()
-        .filter(function () {
-          return this.nodeType === 3;
-        })
-        .eq(0)
-        .wrap('<h4 class="search-filter-results-heading"></h4>');
-    }
-  });
-})(jQuery);
-*/
