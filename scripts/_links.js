9ee9aa7c9539bc367b6b0e583f6eff20b6070fb5
ivanqtbipocdesign <ivan@qtbipoc.design>
2022-05-30 13:50:26 -0700

    Chore: Add comment

--- scripts/_links.js
+++ scripts/_links.js
@@ -2,6 +2,7 @@
 
 
   /* External link icon */
+  // This code adds the `rel="external"` attribute, and needs CSS to add the actual icon
 
   $('a').each(() => true).not('a:has(img), a[href^="/"], a[href^="#"], a[href^="https://qtbipoc.design"], a[href^="mailto"], a[href^="tel"], #footer-custom .nectar_icon a, #wpadminbar *').attr('rel','external');
     
