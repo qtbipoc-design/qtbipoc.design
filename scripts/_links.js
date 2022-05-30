a43595fc6d008302367c4696dc74ecef8af01046
ivanqtbipocdesign <ivan@qtbipoc.design>
2022-05-30 14:30:32 -0700

    Style: Add "staging" to ignore list for icon

--- scripts/_links.js
+++ scripts/_links.js
@@ -4,6 +4,6 @@
   /* External link icon */
   // This code adds the `rel="external"` attribute, and needs CSS to add the actual icon
 
-  $('a').each(() => true).not('a:has(img), a[href^="/"], a[href^="#"], a[href^="https://qtbipoc.design"], a[href^="mailto"], a[href^="tel"], #footer-custom .nectar_icon a, #wpadminbar *').attr('rel','external');
+  $('a').each(() => true).not('a:has(img), a[href^="/"], a[href^="#"], a[href^="https://qtbipoc.design"], a[href^="https://staging"], a[href^="mailto"], a[href^="tel"], #footer-custom .nectar_icon a, #wpadminbar *').attr('rel','external');
     
 }))(jQuery);
