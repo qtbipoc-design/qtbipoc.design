db85a519cada68ae60a86cb0d7739bc966e8bd7c
ivanqtbipocdesign <ivan@qtbipoc.design>
2022-08-25 14:59:15 -0700

    Fix: Wrap JS in jquery ready script

--- scripts/directory.js
+++ scripts/directory.js
@@ -1,17 +1,22 @@
-/* 
- * Automatically detect whether bio is truncated so page can display a "Read more" button 
- * From: https://paulbakaus.com/tutorials/css/multiline-truncated-text-with-show-more-button-with-just-css/
-*/
+(($) => $(() => {
+  /* 
+   * Automatically detect whether bio is truncated so page can display a "Read more" button 
+   * From: https://paulbakaus.com/tutorials/css/multiline-truncated-text-with-show-more-button-with-just-css/
+   */
 
-console.log("Hello directory js");
-const memberBioParagraphs = document.querySelectorAll('.directory-result-member-bio span');
-const contentObserver = new ResizeObserver(entries => {
-  for (let entry of entries) {
-    entry.target.classList[entry.target.scrollHeight > entry.contentRect.height ? 'add' : 'remove']('truncated');
-    console.log(entry);
-  }
-});
+  const memberBioParagraphs = document.querySelectorAll('.directory-result-member-bio span');
+  const contentObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+      entry.target.classList[entry.target.scrollHeight > entry.contentRect.height ? 'add' : 'remove']('truncated');
+      console.log(entry);
+    }
+  });
+  
+  memberBioParagraphs.forEach(span => {
+    contentObserver.observe(span);
+  });
+  
 
-memberBioParagraphs.forEach(span => {
-  contentObserver.observe(span);
-});
+}))(jQuery);
+
+
