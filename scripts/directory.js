cd5391d02d4017ccf458c3fd4ead21355c6084bb
ivanqtbipocdesign <ivan@qtbipoc.design>
2022-08-25 14:01:11 -0700

    Feature: Observe when member bio is truncated

---
+++ scripts/directory.js
@@ -0,0 +1,15 @@
+/* 
+ * Automatically detect whether bio is truncated so page can display a "Read more" button 
+ * From: https://paulbakaus.com/tutorials/css/multiline-truncated-text-with-show-more-button-with-just-css/
+*/
+
+const memberBioParagraphs = document.querySelectorAll('.directory-result-member-bio p');
+const contentObserver = new ResizeObserver(entries => {
+  for (let entry of entries) {
+    entry.target.classList[entry.target.scrollHeight > entry.contentRect.height ? 'add' : 'remove']('truncated');
+  }
+});
+
+memberBioParagraphs.forEach(p => {
+  contentObserver.observe(p);
+});
