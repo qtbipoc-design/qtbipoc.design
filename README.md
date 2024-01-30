5151687299c0f21d48dc7267c1190216f79aea70
ivanqtbipocdesign <ivan@qtbipoc.design>
2024-01-30 14:55:17 -0800

    Feature: Homepage Hero Carousel Display Logic

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # qtbipoc.design
 qtbipoc.design stylesheets and js
+
+1/30/24 Ivan
+To compile css, enter:
+npm run css
+
+To compile js, enter:
+npm run js
+
+Then stage, commit, and push the changes. (This is presumed VSCode/Terminal is set up already to run the scripts)
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,54 @@
 console.debug('Main Loaded');
+
+const homepageHeroCarouselId = "homepage-hero-carousel";
+
+let homepageHeroCarousel = document.querySelector(`#${homepageHeroCarouselId}`);
+
+let carouselItemsToDelete = [];
+
+function updateHomepageHeroCarousel() {
+	console.debug("carousel_panel_next_event", carousel_panel_next_event);
+
+
+	if (carousel_panel_next_event == false) {
+	document.querySelectorAll(`#${homepageHeroCarouselId} .flickity-slider .cell`).forEach(function(el, i) {
+// 		console.debug("running index", i);
+		if (el.contains(document.getElementById("carousel_panel_next_event"))) { 
+			console.debug(i, el);
+			carouselItemsToDelete.push(i);
+		}
+	});
+	}
+
+
+	console.debug("Carousel Items to Delete", carouselItemsToDelete);
+	
+	// Wait to execute code.
+	setTimeout(() => {
+		let flkty = Flickity.data(`#${homepageHeroCarouselId} .nectar-flickity`);
+		
+		let flktyCellsOriginalCount = flkty.cells.length;
+		console.debug("Homepage Hero Carousel cells count", flktyCellsOriginalCount);
+		
+		if (carousel_panel_next_event == true) {
+			/* Do nothing, keep carousel as it */
+		} else if (carousel_panel_next_event == false && flktyCellsOriginalCount > 1) {
+			/* If there are no events, but there is at least one special slide/cell, remove the standard event cell only */
+		
+			// Taken from: https://stackoverflow.com/questions/61687656/flickity-how-can-i-remove-last-cell 
+			carouselItemsToDelete.forEach(function(el, i) {
+				flkty.remove(flkty.cells[carouselItemsToDelete[i]].element);
+			});
+		} else {
+			/* The only slide was the standard event, and there aren't events coming up, hide entire carousel row */
+			document.querySelector(`#${homepageHeroCarouselId}`).style.display = "none";
+		}
+	}, 1000);
+}
+
+document.addEventListener("DOMContentLoaded", function(event){
+	if (homepageHeroCarousel && typeof carousel_panel_next_event != "undefined") {
+		updateHomepageHeroCarousel();
+	}
+}); // End content loaded
+	
